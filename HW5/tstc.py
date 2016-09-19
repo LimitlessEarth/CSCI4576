2301c3b9c7f139694752e032a581ad9ab90d07fd
Adam Ross <adam@soundwall.com>
2016-09-19 11:33:41 -0600

    more updates

--- HW5/tstc.py
+++ HW5/tstc.py
@@ -18,33 +18,37 @@ for data_file in glob.glob("data/*"):
         data = point.split(" ")
         byte_size.append(float(data[0]))
         timing.append(float(data[1]))
-        
-    """
-    fig = plt.figure()
-    fig.suptitle('AllReduce Benchmarks 16 processes', fontsize=20)
-    ax = fig.add_subplot(1,1,1)
-    ax.plot(byte_size, timing)
-    legend = ax.legend(loc='upper center', shadow=True)
-    #ax.set_yticks(np.arange(0, mean_max+500, 500))
-    ax.set_xscale("log", nonposy='clip')
-    ax.set_yscale("log", nonposy='clip')
-    #ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
-    plt.xlabel('Message Data Size(Bytes)', fontsize=14)
-    plt.ylabel('Benchmrked time (us)', fontsize=14)
-    plt.show()
-    """
 
     n = len(byte_size)
-    meanx = np.mean(byte_size)
-    meany = np.mean(timing)
     stdevx = np.std(byte_size)
     stdevy = np.std(timing)
     sumx = sum(byte_size)
     sumy = sum(timing)
     sumxy = sum([byte_size[i] * timing[i] for i in range(n)])
     sumx2 = sum([x ** 2 for x in byte_size])
-    tc = (n*sumxy - sumx*sumy) / (n*sumx2 - sumx**2)
+    tc = ((n*sumxy) - (sumx*sumy)) / ((n*sumx2) - (sumx**2))
+    ts = (sumy - (tc*sumx)) / n
     print sumy
-    print tc*sumx
-    ts = (sumy - tc*sumx) / n
+    print (tc*sumx)
+    
     print "file ", data_file, "     ts: ", '{0:.15f}'.format(ts), "      tc: ", '{0:.15f}'.format(tc)
+    
+    fig = plt.figure()
+    fig.suptitle('AllReduce Benchmarks 16 processes', fontsize=20)
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(byte_size, timing, "o")
+    
+    x = [2**x for x in range(22)]
+    y = [(ts + tc*a) for a in x ]
+    
+    ax.plot(x, y)
+    legend = ax.legend(loc='upper center', shadow=True)
+    #ax.set_yticks(np.arange(0, mean_max+500, 500))
+    #ax.set_xscale("log", nonposy='clip')
+    #ax.set_yscale("log", nonposy='clip')
+    #ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.2e'))
+    plt.xlabel('Message Data Size(Bytes)', fontsize=14)
+    plt.ylabel('Benchmrked time (us)', fontsize=14)
+    plt.show()
+    
+
