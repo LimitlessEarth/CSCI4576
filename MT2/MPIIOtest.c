f3c129077770f7c3bebca96d3e2ed7dbd98abb25
Adam Ross <adam@soundwall.com>
2016-10-08 16:29:40 -0600

    clean test

--- MT2/MPIIOtest.c
+++ MT2/MPIIOtest.c
@@ -4,26 +4,26 @@
 int main(int argc, char **argv){
     int i, rank, size, offset, nints, N=16;
     int buf[N];
-    MPI_File fhw;
+    MPI_File out_file;
     MPI_Status status;
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
-    for ( i=0;i<N;i++){
-        buf[i] = i ;
+    for (i = 0; i < (N / size); i++){
+        buf[i] = i + ((N / size) * rank);
     }
     
-    offset = rank*(N/size)*sizeof(int);
+    offset = rank * (N / size) * sizeof(int);
     
-    MPI_File_open(MPI_COMM_WORLD, "home/adamross/CSCI4576/MT2/datafile", MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &fhw);
+    MPI_File_open(MPI_COMM_WORLD, "datafile", MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &out_file);
     
     printf("\nRank: %d, Offset: %d\n", rank, offset);
     
-    MPI_File_set_view(fhw, offset, MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
-    MPI_File_write(fhw, buf, (N/size), MPI_INT, &status);
-    MPI_File_close(&fhw);
+    MPI_File_set_view(out_file, offset, MPI_INT, MPI_INT, "native", MPI_INFO_NULL);
+    MPI_File_write(out_file, buf, (N/size), MPI_INT, &status);
+    MPI_File_close(&out_file);
     
     MPI_Finalize();
     return 0;
