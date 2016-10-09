a41ac31c3b58510b29398512ac8dc5996a588bfd
Adam Ross <adam@soundwall.com>
2016-10-08 21:57:00 -0600

    delete old files

--- MT2/remake_comet.sh
+++ MT2/remake_comet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git pull && make clean && make && cp RossAdam_MT2 ../../bin && sbatch batch_files/RossAdam_testing.sh && watch squeue -u adamross && cat "dev/comet_out/$(ls -1rt dev/comet_out/ | tail -n1)" && for f in '$(ls /animation2/*.pgm | sort -n -t . -k 1)'; do xxd -p -c 16 ${f}; sleep 0.5; done
+git pull && make clean && make && cp RossAdam_MT2 ../../bin && /oasis/scratch/comet/adamross/temp_project/ && sbatch batch_files/RossAdam_testing.sh && watch squeue -u adamross && cat "dev/comet_out/$(ls -1rt dev/comet_out/ | tail -n1)" 
