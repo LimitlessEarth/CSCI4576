73a0610b5edee19db45a442eeece218035ea3369
Adam Ross <adam@soundwall.com>
2016-10-08 20:18:29 -0600

    not file

--- MT2/RossAdam_MT2.c
+++ MT2/RossAdam_MT2.c
@@ -257,9 +257,9 @@ int main(int argc, char* argv[]) {
         sprintf(frame, "/oasis/scratch/comet/adamross/temp_project/%d.pgm", n);
         MPI_File_open(MPI_COMM_WORLD, frame, MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &out_file);
         
-        MPI_File_set_view(file, 0,  MPI_UNSIGNED_CHAR, MPI_UNSIGNED_CHAR, "native", MPI_INFO_NULL);
+        MPI_File_set_view(out_file, 0,  MPI_UNSIGNED_CHAR, MPI_UNSIGNED_CHAR, "native", MPI_INFO_NULL);
         //write header
-        MPI_File_write(file, &header, 15, MPI_CHAR, MPI_STATUS_IGNORE);
+        MPI_File_write(out_file, &header, 15, MPI_CHAR, MPI_STATUS_IGNORE);
 
         MPI_File_set_view(out_file, rank * local_width, MPI_UNSIGNED_CHAR, darray, "native", MPI_INFO_NULL);
         MPI_File_write_all(out_file, env_a, (local_height * local_width), MPI_INT, &status);
