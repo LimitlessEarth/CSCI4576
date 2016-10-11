302af8cf1b4963fbf25f66c82271609ae67910ba
Adam Ross <adam@soundwall.com>
2016-10-11 12:03:32 -0600

    testing

--- MT2/RossAdam_MT2.c
+++ MT2/RossAdam_MT2.c
@@ -368,8 +368,11 @@ int main(int argc, char* argv[]) {
         //MPI_File_set_view(out_file, 15 + rank * local_width + local_width, MPI_UNSIGNED_CHAR, darray, "native", MPI_INFO_NULL);
         MPI_File_set_view(out_file, 15 + rank * local_width, MPI_UNSIGNED_CHAR, darray, "native", MPI_INFO_NULL);
         //MPI_File_write(out_file, env_a, (local_height * local_width), ext_array, &status);
-        MPI_File_write(out_file, &env_a[local_width + 1], 1, ext_array, &status);
         pprintf("I am at the middle 2 of writing data.\n");
+        
+        MPI_File_write(out_file, &env_a[local_width + 1], 1, ext_array, &status);
+        pprintf("I am at the middle 3 of writing data.\n");
+        
         
         MPI_File_close(&out_file);
         pprintf("I am at the end of writing data.\n");
