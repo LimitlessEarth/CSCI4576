ccbe8d58332fd9fe9674877dceebad0dd5d1944c
Adam Ross <adam@soundwall.com>
2016-10-11 20:40:20 -0600

    testing

--- MT2/batch_files/RossAdam_testing.sh
+++ MT2/batch_files/RossAdam_testing.sh
@@ -3,8 +3,8 @@
 #SBATCH --output="dev/comet_out/RossAdam_MT2test.%j.%N.out"  
 #SBATCH --partition=compute  
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=4
 #SBATCH --export=ALL  
 #SBATCH -t 00:15:00
 
-ibrun RossAdam_MT2 -d 1 -i input_files/glider.pgm -n 10 -c 5
+ibrun RossAdam_MT2 -d 1 -i input_files/glider_gun.pgm -n 50 -c 10
