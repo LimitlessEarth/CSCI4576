de288bf4eae2d7a4a29e436c0b3a9a836312d1e2
Adam Ross <adam@soundwall.com>
2016-10-24 17:09:37 -0600

    n->N

--- MT3/batch_files/RossAdam_MT3_stampede.sh
+++ MT3/batch_files/RossAdam_MT3_stampede.sh
@@ -4,8 +4,8 @@
 #SBATCH -o RossAdam_MT3_DISTTYPE_SYNCTYPE_NPNUM_WORLDSIZE.%j.%N.out
 #SBATCH -e taccError.e%j
 #SBATCH -p normal
-#SBATCH -n NODEVAR
 #SBATCH -n TASKVAR
+#SBATCH -N NODEVAR
 #SBATCH -t 00:15:00
 
 ibrun RossAdam_MT3SYNCFLAG -s WORLDSIZE -d DISTNUM -i input_files/conways_input.pgm -n ITERNUM
