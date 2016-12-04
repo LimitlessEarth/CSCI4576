97b9062c9bb761dfac84d48986726fba6e2e3954
Adam Ross <adam@soundwall.com>
2016-12-04 00:13:28 -0700

    format

--- PROJ/nbody_cuda.cu
+++ PROJ/nbody_cuda.cu
@@ -7,89 +7,94 @@
 #define SOFTENING 1e-9f
 
 
-typedef struct { float4 *pos, *vel; } BodySystem;
+typedef struct { float4 *pos, *vel; } Particle;
 
-void randomizeBodies(float *data, int n) {
-  for (int i = 0; i < n; i++) {
-    data[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-  }
+void initialize_particles(float *data, int n) {
+    for (int i = 0; i < n; i++) {
+        data[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+    }
 }
 
 __global__
-void bodyForce(float4 *p, float4 *v, float dt, int n) {
-  int i = blockDim.x * blockIdx.x + threadIdx.x;
-  if (i < n) {
-    float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
+void body_force(float4 *p, float4 *v, float dt, int n) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < n) {
+        float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
 
-    for (int tile = 0; tile < gridDim.x; tile++) {
-      __shared__ float3 spos[BLOCK_SIZE];
-      float4 tpos = p[tile * blockDim.x + threadIdx.x];
-      spos[threadIdx.x] = make_float3(tpos.x, tpos.y, tpos.z);
-      __syncthreads();
+        for (int tile = 0; tile < gridDim.x; tile++) {
+            __shared__ float3 spos[BLOCK_SIZE];
+            float4 tpos = p[tile * blockDim.x + threadIdx.x];
+            spos[threadIdx.x] = make_float3(tpos.x, tpos.y, tpos.z);
+            __syncthreads();
 
-      #pragma unroll
-      for (int j = 0; j < BLOCK_SIZE; j++) {
-        float dx = spos[j].x - p[i].x;
-        float dy = spos[j].y - p[i].y;
-        float dz = spos[j].z - p[i].z;
-        float distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
-        float invDist = rsqrtf(distSqr);
-        float invDist3 = invDist * invDist * invDist;
+            #pragma unroll
+            for (int j = 0; j < BLOCK_SIZE; j++) {
+                float dx = spos[j].x - p[i].x;
+                float dy = spos[j].y - p[i].y;
+                float dz = spos[j].z - p[i].z;
+                float dist_sqr = dx*dx + dy*dy + dz*dz + SOFTENING;
+                float inv_dist = rsqrtf(dist_sqr);
+                float inv_dist3 = inv_dist * inv_dist * inv_dist;
 
-        Fx += dx * invDist3; Fy += dy * invDist3; Fz += dz * invDist3;
-      }
-      __syncthreads();
+                Fx += dx * inv_dist3; 
+                Fy += dy * inv_dist3; 
+                Fz += dz * inv_dist3;
+            }
+            __syncthreads();
+        }
+
+        v[i].x += Fx * dt; 
+        v[i].y += Fy * dt; 
+        v[i].z += Fz * dt;
     }
-
-    v[i].x += dt*Fx; v[i].y += dt*Fy; v[i].z += dt*Fz;
-  }
 }
 
 int main(const int argc, const char** argv) {
-  
-  int nBodies = 500000;
-  if (argc > 1) nBodies = atoi(argv[1]);
-  
-  const float dt = 0.01f; // time step
-  const int nIters = 100;  // simulation iterations
-  
-  int bytes = 2*nBodies*sizeof(float4);
-  float *buf = (float*)malloc(bytes);
-  BodySystem p = { (float4*)buf, ((float4*)buf) + nBodies };
+    
+    int                 num_part            = 500000;
+    int                 bytes               = 2 * num_part * sizeof(float4);
+    const int           num_iter            = 100;    // simulation iterations   
+    const float         dt                  = 0.01f;    // time step
+    int                 nBlocks, frame, i;
+    float               *buf, *d_buf;
+    double              total_time, avg_time;
+    
+    buf = (float*) malloc(bytes);
+    Particle Host_Particle = { (float4*)buf, ((float4*)buf) + num_part };
 
-  randomizeBodies(buf, 8*nBodies); // Init pos / vel data
+    initialize_particles(buf, 8 *num_part); // Init pos / vel data
 
-  float *d_buf;
-  cudaMalloc(&d_buf, bytes);
-  BodySystem d_p = { (float4*)d_buf, ((float4*)d_buf) + nBodies };
+    cudaMalloc(&d_buf, bytes);
+    Particle Device_Particle = { (float4*) d_buf, ((float4*)d_buf) + num_part };
 
-  int nBlocks = (nBodies + BLOCK_SIZE - 1) / BLOCK_SIZE;
-  double totalTime = 0.0; 
+    nBlocks = (num_part + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    total_time = 0.0; 
 
-  for (int iter = 1; iter <= nIters; iter++) {
-    StartTimer();
+    for (frame = 1; frame <= num_iter; frame++) {
+        StartTimer();
 
-    cudaMemcpy(d_buf, buf, bytes, cudaMemcpyHostToDevice);
-    bodyForce<<<nBlocks, BLOCK_SIZE>>>(d_p.pos, d_p.vel, dt, nBodies);
-    cudaMemcpy(buf, d_buf, bytes, cudaMemcpyDeviceToHost);
+        cudaMemcpy(d_buf, buf, bytes, cudaMemcpyHostToDevice);
+        body_force<<<nBlocks, BLOCK_SIZE>>>(Device_Particle.pos, Device_Particle.vel, dt, num_part);
+        cudaMemcpy(buf, d_buf, bytes, cudaMemcpyDeviceToHost);
 
-    for (int i = 0 ; i < nBodies; i++) { // integrate position
-      p.pos[i].x += p.vel[i].x*dt;
-      p.pos[i].y += p.vel[i].y*dt;
-      p.pos[i].z += p.vel[i].z*dt;
+        for (i = 0 ; i < num_part; i++) { // integrate position
+            Host_Particle.pos[i].x += Host_Particle.vel[i].x * dt;
+            Host_Particle.pos[i].y += Host_Particle.vel[i].y * dt;
+            Host_Particle.pos[i].z += Host_Particle.vel[i].z * dt;
+        }
+
+        const double time_elapsed = GetTimer() / 1000.0;
+        if (frame > 1) { // First iter is warm up
+            total_time += time_elapsed; 
+        }
+        printf("Iteration %d: %.20f seconds\n", iter, time_elapsed);
     }
+    avg_time = total_time / (double) (num_iter-1); 
 
-    const double tElapsed = GetTimer() / 1000.0;
-    if (iter > 1) { // First iter is warm up
-      totalTime += tElapsed; 
-    }
-    printf("Iteration %d: %.20f seconds\n", iter, tElapsed);
-  }
-  double avgTime = totalTime / (double)(nIters-1); 
-
-  printf("%d, %0.3f\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
-  //printf("Average rate for iterations 2 through %d: %.3f +- %.3f steps per second.\n", nIters, rate);
-  printf("%d Bodies: average %0.3f Billion Interactions / second\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
-  free(buf);
-  cudaFree(d_buf);
+    printf("%d, %0.3f\n", num_part, 1e-9 * num_part * num_part / avg_time);
+    //printf("Average rate for iterations 2 through %d: %.3f +- %.3f steps per second.\n", num_iter, rate);
+    printf("%d Bodies: average %0.3f Billion Interactions / second\n", num_part, 1e-9 * num_part * num_part / avg_time);
+    
+    free(buf);
+    cudaFree(d_buf);
 }
