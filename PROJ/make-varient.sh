5877d5ff874062bf5e2a8a0af7bcb321dffb68aa
Adam Ross <adam@soundwall.com>
2016-12-06 21:07:06 -0700

    mre

--- PROJ/make-varient.sh
+++ PROJ/make-varient.sh
@@ -6,13 +6,13 @@ sed -i -e "s/REPLACE/$1/g" nbody_openmp_var.c
 mpicc -g -Wall -std=c99 -fopenmp -lm -c -o nbody_openmp_var.o nbody_openmp_var.c
 mpicc -o nbody_openmp_var nbody_openmp_var.o pprintf.o helper.o -fopenmp -lm
 
-if [ ! -d "$DIRECTORY" ]; then
+if [ ! -d openmp_block ]; then
     mkdir openmp_block
 fi
 
 mv nbody_openmp_var openmp_block/nbody_openmp$1
 sed -i -e "s/$1/REPLACE/g" nbody_openmp_var.c
 
-if [ -d "$DIRECTORY" ]; then
+if [ -d ../../bin ]; then
     cp openmp_block/nbody_openmp$1 ../../bin/.
 fi
