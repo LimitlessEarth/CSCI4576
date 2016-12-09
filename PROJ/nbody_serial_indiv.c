b2ca010805a1ab8a40a7aee8d47b8fdba5ebc2df
Adam Ross <adam@soundwall.com>
2016-12-08 20:28:11 -0700

    unrolled

--- PROJ/nbody_serial_indiv.c
+++ PROJ/nbody_serial_indiv.c
@@ -22,7 +22,7 @@ void swap_dubs(double **a, double **b) {
 
 int main (int argc, char** argv) {
     double              start, end, start_writing, end_writing, start_tot, end_tot;    
-    double              *a, *dist;
+    double              a, dist;
     double              *xa, *ya, *za, *xb, *yb, *zb;
     double              *xva, *yva, *zva, *xvb, *yvb, *zvb; 
     double              *dp, *ap, *mass;
@@ -35,11 +35,8 @@ int main (int argc, char** argv) {
     globals_init();
     parse_args(argc, argv);
         
-    dp = (double *) calloc (12, sizeof(double));
-    ap = (double *) calloc (12, sizeof(double));
-    
-    dist = (double *) calloc (4, sizeof(double));
-    a = (double *) calloc (4, sizeof(double));
+    dp = (double *) calloc (3, sizeof(double));
+    ap = (double *) calloc (3, sizeof(double));
     
     xa = (double *) calloc(num_part, sizeof(double));
     ya = (double *) calloc(num_part, sizeof(double));
@@ -87,79 +84,26 @@ int main (int argc, char** argv) {
         for (i = 0; i < num_part; i++) { // for particle i
     
             ap[0] = 0, ap[1] = 0, ap[2] = 0;
-            for (j = 0; j < num_part; j += 4) { // calculate force based on all other particles
+            for (j = 0; j< num_part; j++) { // calculate force based on all other particles
                 dp[0] = xa[j] - xa[i];
                 dp[1] = ya[j] - ya[i];
                 dp[2] = za[j] - za[i];
-                
-                dp[3] = xa[j + 1] - xa[i];
-                dp[4] = ya[j + 1] - ya[i];
-                dp[5] = za[j + 1] - za[i];
-                
-                dp[6] = xa[j + 2] - xa[i];
-                dp[7] = ya[j + 2] - ya[i];
-                dp[8] = za[j + 2] - za[i];
-                
-                dp[9] = xa[j + 3] - xa[i];
-                dp[10] = ya[j + 3] - ya[i];
-                dp[11] = za[j + 3] - za[i];
         
-                dist[0] = sqrt(dp[0] * dp[0] + dp[1] * dp[1] + dp[2] * dp[2]) + 1;
-                dist[1] = sqrt(dp[3] * dp[3] + dp[4] * dp[4] + dp[5] * dp[5]) + 1;
-                dist[2] = sqrt(dp[6] * dp[6] + dp[7] * dp[7] + dp[8] * dp[8]) + 1;
-                dist[3] = sqrt(dp[9] * dp[9] + dp[10] * dp[10] + dp[11] * dp[11]) + 1;
-                
+                dist = sqrt(dp[0] * dp[0] + dp[1] * dp[1] + dp[2] * dp[2]) + 1;
         
                 if (dist > DOMAIN_SIZE) {
                     continue;
                 }
-                if (dist0 > DOMAIN_SIZE) {
-                    continue;
-                }
-                if (dist1 > DOMAIN_SIZE) {
-                    continue;
-                }
-                if (dist2 > DOMAIN_SIZE) {
-                    continue;
-                }
         
-                a[0] = (G * mass[j]) / (dist[0] * dist[0] * dist[0] * EPS);
-                a[1] = (G * mass[j + 1]) / (dist[1] * dist[1] * dist[1] * EPS);
-                a[2] = (G * mass[j + 2]) / (dist[2] * dist[2] * dist[2] * EPS);
-                a[3] = (G * mass[j + 3]) / (dist[3] * dist[3] * dist[3] * EPS);
-                
+                a = (G * mass[j]) / (dist * dist * dist * EPS);
         
                 ap[0] += a * dp[0]; /* accumulate the acceleration from gravitational attraction */
                 ap[1] += a * dp[1];
                 ap[2] += a * dp[2];
-                
-                ap[3] += a * dp[3]; /* accumulate the acceleration from gravitational attraction */
-                ap[4] += a * dp[4];
-                ap[5] += a * dp[5];
-                
-                ap[6] += a * dp[6]; /* accumulate the acceleration from gravitational attraction */
-                ap[7] += a * dp[7];
-                ap[8] += a * dp[8];
-                
-                ap[9] += a * dp[9]; /* accumulate the acceleration from gravitational attraction */
-                ap[10] += a * dp[10];
-                ap[11] += a * dp[11];
         
                 xva[i] += dt * ap[0]; /* update velocity of particle "i" */
                 yva[i] += dt * ap[1];
                 zva[i] += dt * ap[2];
-                
-                xva[i] += dt * ap[3]; /* update velocity of particle "i" */
-                yva[i] += dt * ap[4];
-                zva[i] += dt * ap[5];
-                
-                xva[i] += dt * ap[6]; /* update velocity of particle "i" */
-                yva[i] += dt * ap[7];
-                zva[i] += dt * ap[8];
-                
-                xva[i] += dt * ap[9]; /* update velocity of particle "i" */
-                yva[i] += dt * ap[10];
-                zva[i] += dt * ap[11];
             }
     
             xb[i] = xa[i] + dt * xva[i]; /* update position of particle "i" */
